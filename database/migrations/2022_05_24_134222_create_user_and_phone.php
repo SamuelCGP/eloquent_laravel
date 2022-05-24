9ea56e4c82f9e8f4959165a19de0faa83114ec82
SamuelCGP <cgplays51@gmail.com>
2022-05-24 11:07:36 -0300

    first commit

---
+++ database/migrations/2022_05_24_134222_create_user_and_phone.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('my_users', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome");
+            $table->timestamps();
+        });
+        Schema::create('user_phone', function (Blueprint $table) {
+            $table->id();
+            $table->string("phone_number");
+            $table->unsignedBigInteger("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_and_phone');
+    }
+};
